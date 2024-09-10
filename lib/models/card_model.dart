6adf896a83571c25833a3788c4dc1828ebafee96
astroerik <dani008molina@gmail.com>
2024-09-09 20:22:26 -0500

    First

---
+++ lib/models/card_model.dart
@@ -0,0 +1,28 @@
+class CardModel {
+  final int id;
+  final String name;
+  final String type;
+  final String desc;
+  final String imageUrl;
+  final double price;
+
+  CardModel({
+    required this.id,
+    required this.name,
+    required this.type,
+    required this.desc,
+    required this.imageUrl,
+    required this.price,
+  });
+
+  factory CardModel.fromJson(Map<String, dynamic> json) {
+    return CardModel(
+      id: json['id'],
+      name: json['name'],
+      type: json['type'],
+      desc: json['desc'],
+      imageUrl: json['card_images'][0]['image_url'],
+      price: double.parse(json['card_prices'][0]['tcgplayer_price']),
+    );
+  }
+}
